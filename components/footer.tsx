b11a0a5930e9e31558f0e6d831114fe9d8c47883
luxumbra <me@luxumbra.dev>
2023-07-12 02:51:05 +0100

    prettier footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 import styles from "./footer.module.css"
 import packageJSON from "../package.json"
-import { Box, Divider, ListItem, UnorderedList, Link } from "@chakra-ui/react"
+import { Box, Divider, ListItem, UnorderedList, Link, Tooltip } from "@chakra-ui/react"
 import Image from './CustomImage'
 import Icon from './CustomIcon'
 import p2pLogo from '../assets/icons/p2pcloud-logo.svg';
@@ -12,8 +12,15 @@ export default function Footer() {
       <Divider />
       <UnorderedList className={styles.navItems} display="flex" alignItems="center" maxW="5xl" mx="auto" listStyleType="none">
         <ListItem className={styles.navItem}>
-        Sponsored by:
-          <Link href="https://usekeyp.com/" isExternal display="inline-flex" alignItems="center"><Image src={keypLogo}  maxH={5} w="auto" ml={1} alt="Powerful Web3 Onboarding ‍for Games and Apps" /></Link>
+          {/* Sponsored by: */}
+          <Tooltip
+            label="Authentication by Keyp"
+            bg="blue.600"
+            rounded={3}
+            hasArrow
+            >
+            <Link href="https://usekeyp.com/" isExternal display="inline-flex" alignItems="center"><Image src={keypLogo} maxH={5} w="auto" ml={1} alt="Powerful Web3 Onboarding ‍for Games and Apps" /></Link>
+            </Tooltip>
         </ListItem>
         <ListItem className={styles.navItem}>
           <Link href="https://github.com/Raid-Guild-Season-VI-Keyp-Integration/next-auth-example/blob/main/README.md" isExternal><Icon icon="file-icons:readthedocs" mr={1} /> Docs</Link>
@@ -22,8 +29,16 @@ export default function Footer() {
           <Link href="https://github.com/Raid-Guild-Season-VI-Keyp-Integration" isExternal><Icon icon="mdi:github" mr={1} /> GitHub</Link>
         </ListItem>
         <ListItem className={styles.navItem}>
-        Hosting by:
-          <Link href="https://p2pcloud.io/" isExternal display="inline-flex" alignItems="center"><Image src={p2pLogo}  maxH={5} w="auto" ml={1} alt="Hosting kindly sponsired by P2PCloud: Decentralized, Secure Cloud Computing." /></Link>
+          {/* Hosting by: */}
+          <Tooltip
+            label="Decentralized Hosting by P2PCloud"
+            textAlign="center"
+            bg="blue.600"
+            rounded={3}
+            hasArrow
+          >
+            <Link href="https://p2pcloud.io/" isExternal display="inline-flex" alignItems="center"><Image src={p2pLogo} maxH={5} w="auto" ml={1} alt="Hosting kindly sponsired by P2PCloud: Decentralized, Secure Cloud Computing." /></Link>
+            </Tooltip>
         </ListItem>
       </UnorderedList>
     </Box>
